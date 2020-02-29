cf43d685767ce61a95351bb17335b77107fa960a
maxwen <max.weninger@gmail.com>
2020-02-29 23:55:29 +0100

    OpenDelta: reset latest full build info after AB update
    and skip checking for delta for AB
    
    Change-Id: I1c30eb3872a4c9c30e9bc198de6532468bb0204d

--- src/org/carbonrom/carbondelta/MainActivity.java
+++ src/org/carbonrom/carbondelta/MainActivity.java
@@ -372,6 +372,9 @@ public class MainActivity extends Activity {
                 mPrefs.edit().putString(UpdateService.PREF_READY_FILENAME_NAME,
                         UpdateService.PREF_READY_FILENAME_DEFAULT).commit();
                 mPrefs.edit().putBoolean(UpdateService.PREF_FILE_FLASH, false).commit();
+                mPrefs.edit().putString(UpdateService.PREF_LATEST_FULL_NAME,
+                        UpdateService.PREF_READY_FILENAME_DEFAULT).commit();
+
                 DrawableCompat.setTint(mCarbonLogo.getDrawable(), ContextCompat.getColor(context, R.color.logo_white));
             } else if (UpdateService.STATE_ACTION_BUILD.equals(state)) {
                 enableCheck = true;
