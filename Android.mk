f41dc66e59ce41eed9c5fddfcf1bb9970537f053
LuK1337 <priv.luk@gmail.com>
2020-09-29 12:28:27 +0200

    Updater: Sign with platform key
    Change-Id: Ia54f30e697d82d522a8dbe8e157b8f9f9e15daa8

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ LOCAL_PACKAGE_NAME := CarbonDelta
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_CERTIFICATE := platform
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
